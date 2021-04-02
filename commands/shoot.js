59cbdaa8867a137bd8546b7612e2b8424d11eda4
Diamond-Guy <alexandru.luca.vasilescu@gmail.com>
2021-04-02 21:37:09 +0300

    Bot Update number 19

--- commands/shoot.js
+++ commands/shoot.js
@@ -4,14 +4,7 @@ const Discord = require('discord.js');
 module.exports = function(message, args) {
 
 
-    // Getting all permissions for a member on a channel.
-    let perms = message.channel.permissionsFor(message.member);
 
-    // Checks for Manage Messages permissions.
-    let can_manage_chans = message.channel.permissionsFor(message.member).has("MANAGE_MESSAGES", false);
-
-    // View permissions as an object (useful for debugging or eval)
-    message.channel.permissionsFor(message.member).serialize(false)
 
 
     var role = member.guild.roles.cache.find(role => role.name === "Shot(Muted)");
