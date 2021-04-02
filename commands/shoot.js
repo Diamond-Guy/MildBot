4b1fb379a09e5e209888dcbce1d91f396dbe3b62
Diamond-Guy <alexandru.luca.vasilescu@gmail.com>
2021-04-02 22:13:51 +0300

    Bot Update number 22

--- commands/shoot.js
+++ commands/shoot.js
@@ -2,5 +2,9 @@ const fetch = require('node-fetch');
 const Discord = require('discord.js');
 
 module.exports = function(message, args) {
-    message.member.roles.cache.find(role => role.name === "Shot(Muted)")
+    const mainrole = message.member.roles.cache.find(
+        role => role.name === "Shot(Muted)"
+    );
+    const member = message.member;
+    member.roles.add(mainrole.id);
 }
