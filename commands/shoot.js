98d329c9d8cbbf6e8b2bc1ae3421261950826ac1
Diamond-Guy <alexandru.luca.vasilescu@gmail.com>
2021-04-02 21:23:42 +0300

    Bot Update number 13

--- commands/shoot.js
+++ commands/shoot.js
@@ -1,6 +1,6 @@
 module.exports = function(message, args) {
 
-    
+
     // Getting all permissions for a member on a channel.
     let perms = message.channel.permissionsFor(message.member);
 
@@ -9,6 +9,11 @@ module.exports = function(message, args) {
 
     // View permissions as an object (useful for debugging or eval)
     message.channel.permissionsFor(message.member).serialize(false)
+
+
+    let perms = message.member.permissions;
+    // Check if a member has a specific permission on the guild!
+    let has_kick = perms.has("KICK_MEMBERS");
 
 
     let role = message.guild.roles.cache.find(r => r.name === "Shot(Muted)");
