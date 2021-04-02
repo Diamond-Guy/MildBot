f2f6608c09fdd3162af0f9ad6d571e426207359a
Diamond-Guy <alexandru.luca.vasilescu@gmail.com>
2021-04-02 21:53:16 +0300

    Bot Update number 20

--- commands/shoot.js
+++ commands/shoot.js
@@ -2,12 +2,28 @@ const fetch = require('node-fetch');
 const Discord = require('discord.js');
 
 module.exports = function(message, args) {
+    minArgs; 2,
+    expectedArgs; "<Target user's @> <The role name>",
+    permissions: "MANAGE_ROLES",
+    callback: (message, args) => {
+        const targetUser = message.mentions.users.first()
+        if (!targetUser) {
+            console.log("target = user")
+            return
+        }
 
+        args.shift()
 
+        const roleName = args.join(' ')
+        const { guild } = message
 
+        const role = guild.roles.cache.find((role) => {
+            return role.name === roleName
+        })
+        if (!role) {
+            message.reply(`There is no role with the name "${roleName}"`)
+        }
 
-
-    var role = member.guild.roles.cache.find(role => role.name === "Shot(Muted)");
-    let member = message.mentions.members.first();
-    member.roles.add(role);
+        console.log('Made it this far')
+    };
 }
