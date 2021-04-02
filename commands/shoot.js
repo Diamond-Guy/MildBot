871d473d5f3298c194899686173e730b1f553e28
Diamond-Guy <alexandru.luca.vasilescu@gmail.com>
2021-04-02 21:12:41 +0300

    Bot Update number ten

--- commands/shoot.js
+++ commands/shoot.js
@@ -1,4 +1,5 @@
 module.exports = function(message, args) {
     let mutedRole = message.guid.roles.cache.find(r => r.name === "Shot(Muted)");
+    let member = message.mentions.members.first();
     member.roles.add(mutedRole).catch(console.error);
 }
