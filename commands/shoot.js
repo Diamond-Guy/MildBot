78cde3a17f048faf4746c625dd30dbc26b6d0a36
Diamond-Guy <alexandru.luca.vasilescu@gmail.com>
2021-04-03 13:36:53 +0300

    Bot Update number 28

--- commands/shoot.js
+++ commands/shoot.js
@@ -1,9 +1,9 @@
 module.exports = function(message, args) {
-message.channel.send("I haven't finished this yet...");
+//message.channel.send("I haven't finished this yet...");
 
-//    const mainrole = message.member.roles.cache.find(
-//        role => role.name === "Shot(Muted)"
-//    );
-//    const member = message.member;
-//    member.roles.add(mainrole.id);
+    const mainrole = message.member.roles.cache.find(
+        role => role.name === "Shot(Muted)"
+    );
+    const member = message.member;
+    member.roles.add(`<@${mainrole}>`);
 }
