57c723032691379ffdbf460a898fa47f3f8d5b33
Diamond-Guy <alexandru.luca.vasilescu@gmail.com>
2021-04-07 19:40:38 +0300

    Bot Update number 47

--- commands/help.js
+++ commands/help.js
@@ -10,7 +10,6 @@ module.exports = function(message, args) {
         .setThumbnail('https://i.imgur.com/wSTFkRM.png')
         .addFields(
             { name: 'Regular field title', value: 'Some value here' },
-            { name: '\u200B', value: '\u200B' },
             { name: 'Inline field title', value: 'Some value here', inline: true },
             { name: 'Inline field title', value: 'Some value here', inline: true },
         )
